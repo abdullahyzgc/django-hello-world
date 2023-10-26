0edf1398bc4619f51cd81c40896c3e9fac6c19b1
Abdullah Yüzgeç <58916581+abdullahyzgc@users.noreply.github.com>
2023-10-26 16:35:07 +0300

    Update views.py

--- example/views.py
+++ example/views.py
@@ -1,15 +1,18 @@
 # example/views.py
 from datetime import datetime
+import platform
 
 from django.http import HttpResponse
 
 def index(request):
     now = datetime.now()
+    version = platform.python_version()
     html = f'''
     <html>
         <body>
             <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
+            <p>The current time is { version }.</p>
+            
         </body>
     </html>
     '''
